69dfe43a9db5951d80f735fef1085f980191efba
RobertBenyamin <robertbenyamino@gmail.com>
2024-09-19 02:02:42 +0700

    Add CORS middleware for handling cross-origin requests

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.27.0
--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"ecommerce-backend/config"
 	"ecommerce-backend/routes"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +13,15 @@ func main() {
 	config.ConnectDatabase()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.UserRoutes(r)
 	routes.ItemRoutes(r)
